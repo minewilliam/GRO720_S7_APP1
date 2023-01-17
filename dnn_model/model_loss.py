31a7b7a2e14c6e56c1d1ab567a19486692ca4974
lafo0701 <lafo0701@usherbrooke.ca>
2023-01-17 11:41:31 -0500

    Added CrossEntropyLoss and MeanSquareErrorLoss functions

--- dnn_model/model_loss.py
+++ dnn_model/model_loss.py
@@ -1,20 +1,29 @@
+import numpy as np
+
 from dnn_framework.loss import Loss as LossBase
 
 
 class CrossEntropyLoss(LossBase):
     """
-    :param x: The input tensor
-    :param target: The target tensor
+    :param x: The input tensor (e.g. model's output)
+    :param target: The target tensor (e.g. true labels)
     :return A tuple containing the loss and the gradient with respect to the input (loss, input_grad)
     """
+
     def calculate(self, x, target):
-        raise NotImplementedError()
+        loss = (-1 * target) * np.log(x) - (1 - target) * np.log(1 - x)
+        input_grad = -1 * (target / x) + ((1 - target) / (1 - x))
+        return loss, input_grad
+
 
 class MeanSquareErrorLoss(LossBase):
     """
-    :param x: The input tensor
-    :param target: The target tensor
+    :param x: The input tensor (e.g. model's output)
+    :param target: The target tensor (e.g. true labels)
     :return A tuple containing the loss and the gradient with respect to the input (loss, input_grad)
     """
+
     def calculate(self, x, target):
-        raise NotImplementedError()
+        loss = np.power((x - target), 2)
+        input_grad = 2 * (x - target)
+        return loss, input_grad
